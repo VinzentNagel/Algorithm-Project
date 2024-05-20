978ac22d6b332ae96ae82d5612f567ddfd987671
VinzentNagel <vinzinagel@gmx.de>
2024-05-20 16:13:30 +0200

    Row class implemented

---
+++ Row.java
@@ -0,0 +1,13 @@
+package Projekt_Implementierung;
+
+public class Row {
+    public String blockchain;
+    public String cryptocoin;
+    public int mining;
+
+    public Row(String blockchain, String cryptocoin, int mining) {
+        this.blockchain = blockchain;
+        this.cryptocoin = cryptocoin;
+        this.mining = mining;
+    }
+}
