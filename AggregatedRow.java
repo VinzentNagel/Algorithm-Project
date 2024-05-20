60f54b0535a099f9a4eeab0ce80578bd9384ade3
VinzentNagel <vinzinagel@gmx.de>
2024-05-20 16:18:00 +0200

    AggregatedRows Class implemented

---
+++ AggregatedRow.java
@@ -0,0 +1,13 @@
+package Projekt_Implementierung;
+
+public class AggregatedRow {
+    public String blockchain;
+    public String cryptocoin;
+    public int mining;
+
+    public AggregatedRow(String blockchain, String cryptocoin, int mining) {
+        this.blockchain = blockchain;
+        this.cryptocoin = cryptocoin;
+        this.mining = mining;
+    }
+}
